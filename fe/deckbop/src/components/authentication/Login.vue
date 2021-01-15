95cc83159c21be1027bc6491b255b66374936d20
Christopher <crisson.christopher@gmail.com>
2021-01-14 19:28:48 -0500

    View Decks implimented

---
+++ fe/deckbop/src/components/authentication/Login.vue
@@ -0,0 +1,70 @@
+<template>
+    <div class="login">
+        <h1>Login</h1>
+        <form id="login-form">
+            <h2>Username</h2>
+            <input type="text" v-model="username">
+            <h2>Password</h2>
+            <input type="password" v-model="password">
+            <button v-on:click.prevent="login(username, password)">login</button>
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Login',
+    data: () => {
+        return {
+            username: '',
+            password: '',
+        }    
+    },
+    props: {
+        // username: String,
+        // password: String,
+    },
+    methods: {
+        login: function (username, password) {
+            console.log(`login with username: ${username} 
+                        password: ${password}`);
+        }
+    }
+
+}
+</script>
+
+<style scoped>
+    .login {
+        display: flex;
+        flex-flow: column;
+        flex: 0 0 100%;
+        align-items: center;
+        border: 1px #747474 solid;
+        border-radius: 2em;
+        margin: 10em 15em;
+        padding: 5em;
+        
+    }
+    h1 {
+        padding-bottom: 2em;
+    }
+    input {
+        margin-bottom: 3em;
+        background: #272727;
+        color: #ffffff;
+        border-top: #747474;
+        border-left: #747474;
+        border-right: #747474;
+    } 
+    input:focus{
+        outline-width: 0;
+    }
+    button {
+        width: 80%;
+        background: #14a76c;
+        border: none;
+        border-radius: 5em;
+        font-size: 1.2em;
+    }
+</style>
---
+++ fe/deckbop/src/components/deckManagement/DeckBox.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="deck-box">
+        <h3>{{deck.deck_name}}</h3>
+    </div>    
+</template>
+
+<script>
+export default {
+    name: "DeckBox",
+    props: ["deck"]
+}
+</script>
+
+<style scoped>
+    .deck-box {
+        border: 1px white solid;
+        width: 10vw;
+        height: 10vh;
+        padding: 5em;
+        text-align: center;
+    }
+</style>
