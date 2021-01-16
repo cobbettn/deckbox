03c5c4fa7223ba8cb9477b0fad777e21cb6d99f9
Christopher <crisson.christopher@gmail.com>
2021-01-16 10:52:29 -0500

    vue-router implimented

--- fe/deckbop/src/main.js
+++ fe/deckbop/src/main.js
@@ -1,8 +1,26 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './App.vue'
 
+// Import components here for routes
+import Login from './components/authentication/Login.vue'
+import ViewDecks from './components/deckManagement/ViewDecks.vue'
+
 Vue.config.productionTip = false
+Vue.use(VueRouter)
+
+const routes = [
+  {path: '/login', component: Login},
+  {path: '/viewDecks', component: ViewDecks},
+  {path: '/', component: ViewDecks},
+]
+
+const router = new VueRouter({
+  routes
+})
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
+
