71dfa346004040af1a06dffa984e5d8623ea0abf
Nathan Cobbett <cobbett.n@gmail.com>
2021-01-18 17:25:43 -0500

    unused constructor

--- be/app/src/main/java/com/deckbop/app/exception/UsernameTakenException.java
+++ be/app/src/main/java/com/deckbop/app/exception/UsernameTakenException.java
@@ -1,9 +1,6 @@
 package com.deckbop.app.exception;
 
 public class UsernameTakenException extends Exception {
-    public UsernameTakenException() {
-        super("User name taken");
-    }
     public UsernameTakenException(String message) {
         super(message);
     }
