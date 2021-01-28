e18d2083998d9d584cae534f97d499185e0e50a7
Christopher <crisson.christopher@gmail.com>
2021-01-28 00:37:33 -0500

    Can search, add and remove from deck in store

--- fe/deckbop/src/store/store.js
+++ fe/deckbop/src/store/store.js
@@ -7,14 +7,28 @@ export default new Vuex.Store({
   state: {
     user: {
       jwt: null
-    }
+    },
+    deck: {cards:[]},
+    searchResults: {data: ""},
   },
   mutations: {
     SET_JWT(state, data) {
       state.user.jwt = data;
+    },
+    SET_SEARCH_RESULTS(state, data){
+      state.searchResults = data;
+    },
+    ADD_TO_DECK(state, data){
+      state.deck.cards = [...state.deck.cards, data]
+    },
+    REMOVE_FROM_DECK(state, data){
+      console.log(data)
+      state.deck.cards.splice(state.deck.cards.findIndex((x) => x == data), 1)
     }
+
   },
   getters: {
-    userJwt: state => state.user.jwt
+    userJwt: state => state.user.jwt,
+    getSearchResults: state => state.searchResults,
   }
 })
