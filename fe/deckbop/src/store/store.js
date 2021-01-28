81790184287b80725bb569acde75e97ead29558e
Nathan Cobbett <cobbett.n@gmail.com>
2021-01-28 08:14:35 -0500

    store update

--- fe/deckbop/src/store/store.js
+++ fe/deckbop/src/store/store.js
@@ -9,6 +9,9 @@ export default new Vuex.Store({
       jwt: null
     }
   },
+  getters: {
+    getUserJwt: state => state.user.jwt
+  },
   mutations: {
     setUserJwt(state, data) {
       state.user.jwt = data
@@ -17,15 +20,12 @@ export default new Vuex.Store({
       state.user.jwt = null
     }
   },
-  getters: {
-    getUserJwt: state => state.user.jwt
-  },
   actions: {
     setUserJwt(context, jwt) {
       context.commit('setUserJwt', jwt)
     },
-    clearUserJwt(context, jwt) {
-      context.commit('clearUserJwt', jwt)
+    clearUserJwt(context) {
+      context.commit('clearUserJwt')
     }
   }
 
