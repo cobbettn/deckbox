8541e01e541c7b6296efeb79688765b536f0e351
Christopher <crisson.christopher@gmail.com>
2021-01-29 21:46:00 -0500

    removed axios calls and loosely wired registeration

--- fe/deckbop/src/config/api.js
+++ fe/deckbop/src/config/api.js
@@ -1,9 +1,12 @@
 const baseUrl = baseLocalURL;
 const baseLocalURL = 'http://localhost:8081';
 const userLogin = '/user/login';
-const userLoginUrl = baseLocalURL + userLogin ;
+const userLoginUrl = baseLocalURL + userLogin;
+const userRegistration = '/user/register';
+const userRegistrationUrl = baseLocalURL + userRegistration;
 
 export {
   baseUrl,
-  userLoginUrl
+  userLoginUrl,
+  userRegistrationUrl,
 };
