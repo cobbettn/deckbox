c167c3f5fdbb936a2a3546408d61cae46f200194
Christopher <crisson.christopher@gmail.com>
2021-01-31 13:09:11 -0500

    post deck in progress

--- fe/deckbop/src/components/deckEditor/DeckInfo.vue
+++ fe/deckbop/src/components/deckEditor/DeckInfo.vue
@@ -21,6 +21,22 @@ export default {
         SidebarNav,
     },
     methods: {
+        saveDeck: function(){
+            const vm = this
+            const reqBody = {
+                name : "update_deck",
+                userId: 1, //vm.$store.getters.user.id,
+                cardList: [{"card_id":"fun","card_quantity":4},{"card_id":"bar","card_quantity":2}] }
+            const reqHeaders = {
+                "Content-Type":"application/json",
+                "Authorization": "Bearer " + vm.$store.getters.userJwt
+            }
+            this.$http.post(
+                "post-url",
+                reqBody,
+                reqHeaders
+            )
+        }
     },
     computed:{
         deckImage: function() {
